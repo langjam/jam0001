2b7fb7cc772f3f35d55fbfd2de6c094918f05a25
jonathan <jonathantbrouwer@gmail.com>
2021-08-20 22:17:14 +0200

    add pub

--- Boole/train/src/ast.rs
+++ Boole/train/src/ast.rs
@@ -1,33 +1,33 @@
 use crate::operations::Operation;
 
 pub struct Program {
-    trains: Vec<Train>,
-    stations: Vec<Station>,
+    pub trains: Vec<Train>,
+    pub stations: Vec<Station>,
 }
 
 pub struct Train {
-    start: Target,
-    first_class_passengers: Vec<FirstClassPassenger>,
-    second_class_passengers: Vec<SecondClassPassenger>
+    pub start: Target,
+    pub first_class_passengers: Vec<FirstClassPassenger>,
+    pub second_class_passengers: Vec<SecondClassPassenger>
 }
 
 pub struct FirstClassPassenger {
-    name: String,
-    data: String,
+    pub name: String,
+    pub data: String,
 }
 
 pub struct SecondClassPassenger {
-    name: String,
-    data: i64
+    pub name: String,
+    pub data: i64
 }
 
 pub struct Station {
-    name: String,
-    operation: Operation,
-    output: Vec<Target>
+    pub name: String,
+    pub operation: Operation,
+    pub output: Vec<Target>
 }
 
 pub struct Target {
-    station: String,
-    track: usize,
+    pub station: String,
+    pub track: usize,
 }
