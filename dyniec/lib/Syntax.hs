f8af46f0f5457f17dc5a33778bb07dfb3d23b0cb
Paweł Dybiec <pawel@dybiec.info>
2021-08-20 19:45:19 +0100

    dyniec: Syntax

--- dyniec/lib/Syntax.hs
+++ dyniec/lib/Syntax.hs
@@ -1,2 +1,23 @@
-module Syntax
-where
+module Syntax where
+
+import qualified Data.Text as T
+
+data Expr
+    = ExPrimop T.Text
+    | ExString T.Text
+    | ExNum Integer
+    | ExAbst T.Text Expr
+    | ExApp Expr Expr
+    | ExAnnot Expr Expr
+    | ExLet T.Text Expr Expr
+    | ExUnit
+    | ExTyAnnot Expr Type
+data Type
+    = TyStr
+    | TyNum
+    | TyArrow Type Type
+    | TyUnit
+data Module = Module
+    { exports :: [T.Text]
+    , declarations :: [(T.Text, Expr)]
+    }
