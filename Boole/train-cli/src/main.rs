f7446220c748b6badf92e13cfb4e7633fc60143e
jonay2000 <jonabent@gmail.com>
2021-08-20 22:08:45 +0200

    gitignore and lci

--- Boole/train-cli/src/main.rs
+++ Boole/train-cli/src/main.rs
@@ -1,5 +1,14 @@
-
+use clap::{App, Arg};
+use clap::crate_authors;
 
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Train")
+        .version("1.0")
+        .author(crate_authors!("\n"))
+        .about("Take a train to your destination do some computation along the way")
+        .arg(Arg::with_name("program")
+            .help("a program of your train network"))
+        .get_matches();
+
+
 }
