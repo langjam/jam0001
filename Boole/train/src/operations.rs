4039244885d7a96831812bfc8bc56843aebeaa06
jonay2000 <jonabent@gmail.com>
2021-08-20 21:52:40 +0200

    idea

---
+++ Boole/train/src/operations.rs
@@ -0,0 +1,21 @@
+
+pub enum Operation {
+    Nothing,
+    Print,
+    Input,
+
+    Duplicate,
+    Rotate,
+    DeleteTop,
+    Transfer,
+
+
+
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+
+    Delete,
+}
