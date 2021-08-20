910e5c347f7e5d440d6545d8974e944b1203db8b
jonathan <jonathantbrouwer@gmail.com>
2021-08-20 22:08:05 +0200

    Ast

--- Boole/train/src/lib.rs
+++ Boole/train/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod operations;
+mod ast;
--- Boole/train/src/operations.rs
+++ Boole/train/src/operations.rs
@@ -3,13 +3,12 @@ pub enum Operation {
     Nothing,
     Print,
     Input,
+    Switch,
 
     Duplicate,
     Rotate,
     DeleteTop,
     Transfer,
-
-
 
     Add,
     Sub,
@@ -18,4 +17,28 @@ pub enum Operation {
     Mod,
 
     Delete,
+}
+
+impl Operation {
+    pub fn input_track_count(&self) -> usize {
+        match self {
+            Operation::Nothing => 1,
+            Operation::Print => 1,
+            Operation::Input => 0,
+            Operation::Switch => 1,
+
+            Operation::Duplicate => 1,
+            Operation::Rotate => 1,
+            Operation::DeleteTop => 1,
+            Operation::Transfer => 2,
+
+            Operation::Add => 2,
+            Operation::Sub => 2,
+            Operation::Mul => 2,
+            Operation::Div => 2,
+            Operation::Mod => 2,
+
+            Operation::Delete => 1,
+        }
+    }
 }
