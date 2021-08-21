276b9a27504a63e94e05d4cd803368be3a6565e1
Jakub Sebek <jamertwo@gmail.com>
2021-08-21 23:03:32 +0200

    New parser integration

--- c_chads/lang/interpreter/interpreter.c
+++ c_chads/lang/interpreter/interpreter.c
@@ -65,35 +65,37 @@ static struct Interpreter_Value execute(struct Vec OF(struct Parser_Node*) *body
 
 static struct Interpreter_Value call(struct Parser_Node* node) {
 
-    struct Interpreter_Value* func = get_var(node->data.call.name);
+    struct Interpreter_Value* func = get_var(pnode_left(node)->data.ident.val);
 
     struct Map* oldframe = intrp.vars;
     struct Map vars = map_new(sizeof(struct Vec));
+
+    struct Vec* args = &pnode_right(node)->children;
 
     struct Interpreter_Value ret = void_val(); 
     switch (func->type) {
         case IT_CFUNC: {
 
-            struct Vec args = vec_new(sizeof(struct Interpreter_Value));
-            for (usize i = 0; i < node->children.size; i += 1) {
-                struct Interpreter_Value val = intrp_run(vec_get(&node->children, i));
-                vec_push(&args, &val);
+            struct Vec params = vec_new(sizeof(struct Interpreter_Value));
+            for (usize i = 0; i < args->size; i += 1) {
+                struct Interpreter_Value arg = intrp_run(vec_get(args, i));
+                vec_push(&params, &arg);
             }
 
             intrp.vars = &vars;
-            ret = func->data.cfunc.func(&args);
-            vec_drop(&args);
+            ret = func->data.cfunc.func(&params);
+            vec_drop(&params);
         } break;
-        case IT_FUNC:
-            for (usize i = 0; i < node->children.size; i++) {
-                struct Interpreter_Value arg = intrp_run(vec_get(&node->children, i));
-                struct Parser_Node* params = vec_get(&func->data.func.ast->children, 0);
-                struct Parser_Node* decl = vec_get(&params->children, i);
-                map_add(&vars, decl->data.decl.name, &arg);
+        case IT_FUNC: {
+            struct Vec* params = &pnode_left(func->data.func.ast)->children;
+            for (usize i = 0; i < args->size; i++) {
+                struct Interpreter_Value arg = intrp_run(vec_get(args, i));
+                struct Parser_Node* fdecl = vec_get(params, i);
+                map_add(&vars, fdecl->data.decl.name, &arg);
             }
             intrp.vars = &vars;
-            ret = execute(&func->data.func.ast->children);
-        break;
+            ret = execute(&pnode_right(func->data.func.ast)->children);
+        } break;
         default:
         break;
     }
@@ -176,5 +178,5 @@ struct Interpreter_Value intrp_run(struct Parser_Node* node) {
 }
 
 struct Interpreter_Value intrp_main() {
-    return execute(&((struct Interpreter_Value*)map_get(&intrp.global, strview_from("main")))->data.func.ast->children);
+    return execute(&pnode_right(((struct Interpreter_Value*)map_get(&intrp.global, strview_from("main")))->data.func.ast)->children);
 }
