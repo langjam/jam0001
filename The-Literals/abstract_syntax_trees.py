75fa9ce365e5446dd105b10438d071e9604f3c63
Lorne Hyde <lorne.ab.hyde@gmail.com>
2021-08-21 21:40:40 +0100

    Add type hints to abstract syntax tree

--- The-Literals/abstract_syntax_trees.py
+++ The-Literals/abstract_syntax_trees.py
@@ -1,31 +1,32 @@
 class Expr:
     pass
 
+class Operand(Expr):
+    pass
 
-class Number(Expr):
-    def __init__(self, value):
+class Number(Operand):
+    def __init__(self, value: int):
         self.value = value
 
     def __repr__(self):
         return f"NUMBER {self.value}"
 
-
-class Variable(Expr):
-    def __init__(self, varname):
+class Variable(Operand):
+    def __init__(self, varname: str):
         self.varname = varname
 
     def __repr__(self):
         return f"VAR {self.varname}"
 
 class Parameter(Expr):
-    def __init__(self, varname):
+    def __init__(self, varname: str):
         self.varname = varname
 
     def __repr__(self):
         return f"PARAM {self.varname}"
 
 class Binop(Expr):
-    def __init__(self, operator, left_operand, right_operand):
+    def __init__(self, operator: str, left_operand: Operand, right_operand: Operand):
         self.operator = operator
         self.left_operand = left_operand
         self.right_operand = right_operand
@@ -35,7 +36,7 @@ class Binop(Expr):
 
 
 class Comparison(Expr):
-    def __init__(self, operator, left_operand, right_operand):
+    def __init__(self, operator: str, left_operand: Operand, right_operand: Operand):
         self.operator = operator
         self.left_operand = left_operand
         self.right_operand = right_operand
@@ -49,7 +50,7 @@ class Stmt:
 
 
 class IfStmt(Stmt):
-    def __init__(self, condition, thenpt):
+    def __init__(self, condition: Expr, thenpt: Stmt):
         self.condition = condition
         self.thenpt = thenpt
 
@@ -58,25 +59,15 @@ class IfStmt(Stmt):
 
 
 class SetStmt(Stmt):
-    def __init__(self, target, value):
+    def __init__(self, target: Variable, value: Expr):
         self.target = target
         self.value = value
 
     def __repr__(self):
         return f"SET {self.target} TO {self.value}"
 
-
-class Program:
-    def __init__(self, funcs, stmts):
-        self.funcs = funcs
-        self.stmts = stmts
-
-    def __repr__(self):
-        return (
-            f"\nPROGRAM \n{self.funcs}\n{self.stmts}")
-
 class FuncHeader:
-    def __init__(self, func_name, params, return_var):
+    def __init__(self, func_name: str, params: "list[Parameter]", return_var: Variable):
         self.func_name = func_name
         self.params = params
         self.return_var = return_var
@@ -84,3 +75,12 @@ class FuncHeader:
     def __repr__(self):
         return (
             f"FUNC (NAME:{self.func_name}, PARAMS:{self.params}, RETURNS:{self.return_var}")
+
+class Program:
+    def __init__(self, funcs: "list[FuncHeader]", stmts):
+        self.funcs = funcs
+        self.stmts = stmts
+
+    def __repr__(self):
+        return (
+            f"\nPROGRAM \n{self.funcs}\n{self.stmts}")
