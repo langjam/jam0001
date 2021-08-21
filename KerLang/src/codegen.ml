f7bcf6a4d6de60a3cadbd801746944b462a31ea8
anima-libera <anim.libera@gmail.com>
2021-08-21 19:02:09 +0200

    Code gen is good now

--- KerLang/src/codegen.ml
+++ KerLang/src/codegen.ml
@@ -15,7 +15,7 @@ let rec ast_params_count (func: ast): int =
   | If(cond, ifcase, elsecase) ->
     max3 (ast_params_count cond) (ast_params_count ifcase) (ast_params_count elsecase)
 
-let op_params_count ?(self_param_count=0) (op: op): int = 
+let op_params_count ?(self_param_count: int = 0) (op: op): int = 
   match op with
   | OUT -> 2
   | ADD -> 2
@@ -35,109 +35,111 @@ let print_params (params_count: int) =
     print_int i;
     print_string " ";
   done;
-  print_string "x";
-  print_int (params_count-1)
+  if params_count >= 1 then begin
+    print_string "x";
+    print_int (params_count-1)
+  end
 
-let rec ast_generate ?(indent_lvl=0) (func: ast) =
+let rec ast_generate ?(self_name: string option = None) ?(indent_lvl: int = 0) (func: ast) =
   match func with
   | Cst(value) -> begin
+      print_string "\n";
       print_indent indent_lvl;
-      print_int value;
-      print_string "\n"
+      print_int value
     end
   | Var(id) -> begin
+      print_string "\n";
       print_indent indent_lvl;
       print_string "x";
-      print_int id;
-      print_string "\n"
+      print_int id
     end
-  | App(op, _) -> begin
+  | App(op, args) -> begin
+      print_string "\n";
       print_indent indent_lvl;
       print_string "(";
-      op_generate ~indent_lvl:(indent_lvl+1) op;
-      print_string "\n";
-      (* TODO *)
-      print_string "\nTODO: args\n";
+      op_generate ~self_name:(self_name) ~indent_lvl:(indent_lvl) op;
+      print_string " ";
+      ast_list_generate ~self_name:(self_name) ~indent_lvl:(indent_lvl+1) args;
       print_string ")"
     end
   | If(cond, ifcase, elsecase) -> begin
-      print_indent indent_lvl;
-      print_string "if\n";
-      ast_generate ~indent_lvl:(indent_lvl+1) cond;
       print_string "\n";
       print_indent indent_lvl;
-      print_string "then\n";
-      ast_generate ~indent_lvl:(indent_lvl+1) ifcase;
+      print_string "if";
+      ast_generate ~self_name:(self_name) ~indent_lvl:(indent_lvl+1) cond;
       print_string "\n";
       print_indent indent_lvl;
-      print_string "else\n";
-      ast_generate ~indent_lvl:(indent_lvl+1) elsecase;
-      print_string "\n"
+      print_string "then";
+      ast_generate ~self_name:(self_name) ~indent_lvl:(indent_lvl+1) ifcase;
+      print_string "\n";
+      print_indent indent_lvl;
+      print_string "else";
+      ast_generate ~self_name:(self_name) ~indent_lvl:(indent_lvl+1) elsecase;
     end
 
-and op_generate ?(indent_lvl=0) (op: op) =
+and ast_list_generate ?(self_name: string option = None) ?(indent_lvl: int = 0) (ast_list: ast list) =
+  match ast_list with
+  | [] -> ()
+  | ast::q -> begin
+      ast_generate ~self_name:(self_name) ~indent_lvl:(indent_lvl) ast;
+      ast_list_generate ~self_name:(self_name) ~indent_lvl:(indent_lvl) q
+    end
+
+and op_generate ?(self_name: string option = None) ?(indent_lvl: int = 0) (op: op) =
   match op with
   | OUT -> begin
-      print_indent indent_lvl;
       print_string "(fun ";
       print_params (op_params_count op);
       print_string " -> ";
-      print_string "\n";
-      print_indent (indent_lvl+1);
-      print_string "print_int x0;\n";
-      print_indent (indent_lvl+1);
-      print_string "x1\n";
-      print_indent indent_lvl;
+      print_string "print_int x0; print_string \"\\n\"; ";
+      print_string "x1";
       print_string ")"
     end
   | ADD -> begin
-      print_indent indent_lvl;
-      print_string "(fun ";
-      print_params (op_params_count op);
-      print_string " -> ";
-      print_string "\n";
-      print_indent (indent_lvl+1);
-      print_string "x0 + x1\n";
-      print_indent indent_lvl;
-      print_string ")"
-    end
-  | MUL -> begin
-      print_indent indent_lvl;
-      print_string "(fun ";
-      print_params (op_params_count op);
-      print_string " -> ";
-      print_string "\n";
-      print_indent (indent_lvl+1);
-      print_string "x0 - x1\n";
-      print_indent indent_lvl;
-      print_string ")"
+      print_string "(+)"
     end
   | SUB -> begin
-      print_indent indent_lvl;
-      print_string "(fun ";
-      print_params (op_params_count op);
-      print_string " -> ";
-      print_string "\n";
-      print_indent (indent_lvl+1);
-      print_string "x0 * x1\n";
-      print_indent indent_lvl;
-      print_string ")"
+      print_string "(-)"
+    end
+  | MUL -> begin
+      print_string "( * )"
     end
   | FUN(Some(name), _) -> begin
       print_string name
     end
   | FUN(None, func) -> begin
-      print_indent indent_lvl;
       print_string "(fun ";
       print_params (op_params_count op);
       print_string " -> ";
-      print_string "\n";
-      ast_generate ~indent_lvl:(indent_lvl+1) func;
-      print_string "\n";
-      print_indent indent_lvl;
+      ast_generate ~self_name:(self_name) ~indent_lvl:(indent_lvl+1) func;
       print_string ")"
     end
   | SELF -> begin
-      (* TODO *)
-      print_string "\nTODO: SELF\n"
+      match self_name with
+      | None -> failwith "were gonna need a self name here"
+      | Some(name) -> print_string name
     end
+
+let function_as_ast_generate ?(indent_lvl: int = 0) (name: string) (func: ast) = 
+  print_indent indent_lvl;
+  print_string "let rec ";
+  print_string name;
+  print_string " = ";
+  ast_generate ~self_name:(Some(name)) ~indent_lvl:(indent_lvl+1) func;
+  print_string "\n"
+
+let function_as_op_generate ?(indent_lvl: int = 0) (op: op) = 
+  match op with
+  | FUN(name_opt, func) -> begin
+      match name_opt with
+      | Some(name) -> begin
+          print_indent indent_lvl;
+          print_string "let rec ";
+          print_string name;
+          print_string " = ";
+          op_generate ~self_name:(Some(name)) ~indent_lvl:(indent_lvl+1) (FUN(None, func));
+          print_string "\n"
+        end
+      | None -> failwith "i aint gonna generate an op with no name"
+    end
+  | _ -> failwith "whats even that"
