54a1ce7432a794f99a4b6f89fb9b0ff54c6b0d9e
Dan Freeman <dfreeman@salsify.com>
2021-08-21 23:30:23 +0200

    Parser

---
+++ dfreeman/src/parser/ast.ts
@@ -0,0 +1,285 @@
+import { Token } from 'feldspar';
+import { unreachable } from '../utils';
+import { NodeDetails } from './combinators';
+
+export type Loc = Readonly<{ offset: number; length: number }>;
+
+export type Declaration = DefDeclaration | ADTDeclaration;
+export type Expression =
+  | NumberLiteral
+  | BooleanLiteral
+  | FunctionExpression
+  | CallExpression
+  | Identifier
+  | ADTConstructor
+  | MatchExpression;
+
+export type Pattern = NumberLiteral | BooleanLiteral | Identifier | ADTConstructorPattern;
+
+export class NumberLiteral {
+  public readonly kind = 'Number';
+  public constructor(public readonly value: number, public readonly loc: Loc) {}
+
+  public static fromToken({ loc }: NodeDetails, token: Token): NumberLiteral {
+    return new NumberLiteral(Number(token.content), loc);
+  }
+}
+
+export class BooleanLiteral {
+  public readonly kind = 'Boolean';
+  public constructor(public readonly value: boolean, public readonly loc: Loc) {}
+
+  public static fromToken({ loc }: NodeDetails, token: Token): BooleanLiteral {
+    return new BooleanLiteral(token.content === 'true', loc);
+  }
+}
+
+export class Identifier {
+  public readonly kind = 'Identifier';
+  public constructor(
+    public readonly value: string,
+    public readonly isMeta: boolean,
+    public readonly metaValue: string | null,
+    public readonly loc: Loc
+  ) {}
+
+  public toString(): string {
+    if (this.isMeta) {
+      return `${this.value}#${this.metaValue}`;
+    } else {
+      return this.value;
+    }
+  }
+
+  public static fromToken({ loc }: NodeDetails, token: Token): Identifier {
+    let hashIndex = token.content.indexOf('#');
+    let isMeta = hashIndex !== -1;
+    let value = token.content;
+    let metaValue = null;
+    if (isMeta) {
+      metaValue = value.slice(hashIndex + 1) || null;
+      value = value.slice(0, hashIndex);
+    }
+
+    return new Identifier(value, isMeta, metaValue, loc);
+  }
+}
+
+export class DefDeclaration {
+  public readonly kind = 'DefDeclaration';
+  public constructor(
+    public readonly name: Identifier,
+    public readonly value: Expression,
+    public readonly loc: Loc
+  ) {}
+
+  public static fromSequence(
+    { loc }: NodeDetails,
+    [, name, , value]: [Token, Identifier, Token, Expression]
+  ): DefDeclaration {
+    return new DefDeclaration(name, value, loc);
+  }
+}
+
+interface ScriptDeclaration {
+  declaration: Declaration;
+  value: Expression | null;
+  meta: SemanticComment | null;
+}
+
+export class Script {
+  public readonly kind = 'Script';
+  public constructor(public readonly declarations: Map<string, ScriptDeclaration>) {}
+
+  public static fromSequence(input: Array<[SemanticComment | null, Declaration]>): Script {
+    let declarations = new Map<string, ScriptDeclaration>();
+    for (let [meta, declaration] of input) {
+      if (declaration.kind === 'DefDeclaration') {
+        let { name, value } = declaration;
+        declarations.set(name.value, { meta, declaration, value });
+      } else if (declaration.kind === 'ADTDeclaration') {
+        let { name, constructors } = declaration;
+        for (let constructor of constructors) {
+          declarations.set(constructor.name.value, { meta: null, declaration, value: constructor });
+        }
+        declarations.set(name.value, { meta, declaration, value: null });
+      } else {
+        unreachable(declaration);
+      }
+    }
+    return new Script(declarations);
+  }
+}
+
+export type TextSegment = { kind: 'TextSegment'; content: string };
+export type ExampleSegment = {
+  kind: 'ExampleSegment';
+  name: string;
+  description: string;
+  value: Expression;
+};
+
+export type CommentSegment = TextSegment | ExampleSegment;
+export class SemanticComment {
+  public readonly kind = 'SemanticComment';
+  public constructor(
+    public readonly segments: ReadonlyArray<CommentSegment>,
+    public readonly loc: Loc
+  ) {}
+
+  public static fromSegments(
+    { loc }: NodeDetails,
+    [, inputSegments]: [
+      commentStart: Token,
+      segments: Array<
+        | Token
+        | [
+            exampleStart: Token,
+            exampleDescription: Token,
+            exampleValue: Expression,
+            exampleEnd: Token
+          ]
+      >,
+      commentEnd: Token
+    ]
+  ): SemanticComment {
+    let segments: Array<CommentSegment> = [];
+    let currentText: TextSegment | undefined;
+
+    for (let segment of inputSegments) {
+      if ('kind' in segment) {
+        let line = segment.content.replace(/^#/, '').trim();
+        if (currentText) {
+          currentText.content += `\n${line}`;
+        } else {
+          currentText = { kind: 'TextSegment', content: line };
+        }
+      } else {
+        if (currentText) {
+          segments.push(currentText);
+          currentText = undefined;
+        }
+
+        let [startToken, descriptionToken, value] = segment;
+        let name = startToken.content.slice(startToken.content.indexOf('#') + 1).trim();
+        let description = descriptionToken.content.trim();
+
+        segments.push({ kind: 'ExampleSegment', name, description, value });
+      }
+    }
+
+    if (currentText) {
+      segments.push(currentText);
+    }
+
+    return new SemanticComment(segments, loc);
+  }
+}
+
+export class FunctionExpression {
+  public readonly kind = 'Function';
+  public constructor(
+    public readonly source: string,
+    public readonly params: ReadonlyArray<Identifier>,
+    public readonly body: ReadonlyArray<Expression>,
+    public readonly loc: Loc
+  ) {}
+
+  public static fromSequence(
+    { source, loc }: NodeDetails,
+    [, params, body]: [Token, Array<Identifier>, Array<Expression>, Token]
+  ): FunctionExpression {
+    return new FunctionExpression(source, params, body, loc);
+  }
+}
+
+export class CallExpression {
+  public readonly kind = 'Call';
+  public constructor(
+    public readonly callee: Expression,
+    public readonly args: ReadonlyArray<Expression>,
+    public readonly loc: Loc
+  ) {}
+
+  public static fromSequence(
+    { loc }: NodeDetails,
+    [callee, , args]: [Expression, Token, Array<Expression>, Token]
+  ): CallExpression {
+    return new CallExpression(callee, args, loc);
+  }
+}
+
+export class ADTDeclaration {
+  public readonly kind = 'ADTDeclaration';
+  public constructor(
+    public readonly name: Identifier,
+    public readonly constructors: ReadonlyArray<ADTConstructor>,
+    public readonly loc: Loc
+  ) {}
+
+  public static fromSequence(
+    { loc }: NodeDetails,
+    [, name, , variants]: [
+      Token,
+      Identifier,
+      Token,
+      Array<[Identifier, Token, Array<Identifier>, Token]>
+    ]
+  ): ADTDeclaration {
+    let structuredVariants = variants.map(([name, , params, rParen]) => {
+      return new ADTConstructor(name, params, {
+        offset: name.loc.offset,
+        length: rParen.offset + rParen.content.length - name.loc.offset,
+      });
+    });
+
+    return new ADTDeclaration(name, structuredVariants, loc);
+  }
+}
+
+export class ADTConstructor {
+  public readonly kind = 'ADTConstructor';
+  public constructor(
+    public readonly name: Identifier,
+    public readonly params: ReadonlyArray<Identifier>,
+    public readonly loc: Loc
+  ) {}
+}
+
+export class ADTConstructorPattern {
+  public readonly kind = 'ADTConstructor';
+  public constructor(
+    public readonly name: Identifier,
+    public readonly params: ReadonlyArray<Pattern>,
+    public readonly loc: Loc
+  ) {}
+
+  public static fromSequence(
+    { loc }: NodeDetails,
+    [name, , args]: [Identifier, Token, Array<Pattern>, Token]
+  ): ADTConstructorPattern {
+    return new ADTConstructorPattern(name, args, loc);
+  }
+}
+
+export type MatchArm = {
+  pattern: Pattern;
+  result: Expression;
+};
+
+export class MatchExpression {
+  public readonly kind = 'Match';
+  public constructor(
+    public readonly subject: Expression,
+    public readonly arms: ReadonlyArray<MatchArm>,
+    public readonly loc: Loc
+  ) {}
+
+  public static fromSequence(
+    { loc }: NodeDetails,
+    [, subject, arms]: [Token, Expression, Array<[Token, Pattern, Token, Expression]>, Token]
+  ): MatchExpression {
+    let matchArms = arms.map(([, pattern, , result]) => ({ pattern, result }));
+    return new MatchExpression(subject, matchArms, loc);
+  }
+}
---
+++ dfreeman/src/parser/index.ts
@@ -0,0 +1,196 @@
+import {
+  alt,
+  Combinator,
+  defer,
+  maybe,
+  Parser,
+  repeat,
+  repeatOne,
+  repeatOneSep,
+  repeatSep,
+  seq,
+  state,
+  token,
+} from 'feldspar';
+import {
+  SemanticComment,
+  Expression,
+  Identifier,
+  NumberLiteral,
+  DefDeclaration,
+  Script,
+  FunctionExpression,
+  CallExpression,
+  ADTDeclaration,
+  Pattern,
+  ADTConstructorPattern,
+  MatchExpression,
+  BooleanLiteral,
+} from './ast';
+import { node } from './combinators';
+
+////////////////////////////////////////////////////////////////
+//// Basic tokens
+////
+
+const ws = token(/\s+/, { priority: 0, silent: true });
+const ignoredComment = token(/#.*(?=\n|$)/, { priority: 0x100, silent: true });
+
+const def = token(/def\b/, { kind: 'DefKeyword' });
+const data = token(/data\b/, { kind: 'DataKeyword' });
+const fun = token(/fun\b/, { kind: 'FunKeyword' });
+const match = token(/match\b/, { kind: 'MatchKeyword' });
+const end = token(/end\b/, { kind: 'EndKeyword' });
+const eq = token('=', { kind: 'Equals' });
+const rArrow = token('->', { kind: 'RArrow' });
+const lParen = token('(', { kind: 'LParen' });
+const rParen = token(')', { kind: 'RParen' });
+const comma = token(',', { kind: 'Comma' });
+const semicolon = token(';', { kind: 'Semicolon' });
+const pipe = token('|', { kind: 'Pipe' });
+
+const identifier = node(
+  Identifier.fromToken,
+  token(/[a-z_][a-z0-9_]*\b(?!#)/i, {
+    kind: 'Identifier',
+    priority: 0x100,
+  })
+);
+
+const metaIdentifier = node(
+  Identifier.fromToken,
+  token(/[a-z_][a-z0-9_]*(?:#(?:[a-z][a-z0-9]*)?)/i, {
+    kind: 'MetaIdentifier',
+    priority: 0x100,
+  })
+);
+
+const number = node(NumberLiteral.fromToken, token(/\d+/, { kind: 'Number' }));
+const boolean = node(BooleanLiteral.fromToken, token(/true\b|false\b/, { kind: 'Boolean' }));
+
+////////////////////////////////////////////////////////////////
+//// Patterns
+////
+
+const pattern: Combinator<Pattern> = defer(() =>
+  alt(identifier, number, boolean, constructorPattern)
+);
+
+const constructorPattern = node(
+  ADTConstructorPattern.fromSequence,
+  seq(identifier, lParen, repeatSep(pattern, comma), rParen)
+);
+
+////////////////////////////////////////////////////////////////
+//// Expressions
+////
+
+const expression: Combinator<Expression> = defer((expr) =>
+  alt(
+    number,
+    boolean,
+    identifier,
+    metaIdentifier,
+    functionExpression,
+    callExpression,
+    matchExpression,
+    seq(lParen, expr, rParen).map((seq) => seq[1])
+  )
+);
+
+const functionExpression = node(
+  FunctionExpression.fromSequence,
+  seq(
+    fun,
+    seq(lParen, repeatSep(identifier, comma), rParen).map((seq) => seq[1]),
+    repeatSep(expression, semicolon),
+    end
+  )
+);
+
+const callExpression = node(
+  CallExpression.fromSequence,
+  seq(expression, lParen, repeatSep(expression, comma), rParen)
+);
+
+const matchExpression = node(
+  MatchExpression.fromSequence,
+  seq(match, expression, repeatOne(seq(pipe, pattern, rArrow, expression)), end)
+);
+
+////////////////////////////////////////////////////////////////
+//// Semantic Comments
+////
+
+const inComment = state('in-comment', { exclusive: true });
+const inExampleCode = state('in-example-code');
+const inExampleDescription = state('in-example-dscription', {
+  exclusive: true,
+});
+
+const commentStart = token(/###\s*/, { kind: 'CommentStart', pushState: inComment });
+const commentLine = token(/.*\n/, { kind: 'CommentLine', requiredState: inComment });
+const commentEnd = token(/\s*###/, { kind: 'CommentEnd', popState: inComment, priority: 0 });
+const commentExampleStart = token(/\s*@example#[a-zA-Z_][a-zA-Z0-9_]*\b/, {
+  kind: 'CommentExample',
+  requiredState: inComment,
+  priority: 0,
+  pushState: inExampleDescription,
+});
+
+const commentExampleDescription = token(/.*\n/, {
+  kind: 'CommentExampleDescription',
+  popState: inExampleDescription,
+  pushState: inExampleCode,
+});
+
+const commentExampleEnd = token(/\s*@end/, {
+  kind: 'CommentExampleEnd',
+  popState: inExampleCode,
+});
+
+const commentExample = seq(
+  commentExampleStart,
+  commentExampleDescription,
+  expression,
+  commentExampleEnd
+);
+const comment = node(
+  SemanticComment.fromSegments,
+  seq(commentStart, repeat(alt(commentLine, commentExample)), commentEnd)
+);
+
+////////////////////////////////////////////////////////////////
+//// Declarations
+////
+
+const defDeclaration = node(DefDeclaration.fromSequence, seq(def, identifier, eq, expression));
+
+const adtDeclaration = node(
+  ADTDeclaration.fromSequence,
+  seq(
+    data,
+    identifier,
+    eq,
+    repeatOneSep(seq(identifier, lParen, repeatSep(identifier, comma), rParen), pipe)
+  )
+);
+
+const declaration = alt(defDeclaration, adtDeclaration);
+
+////////////////////////////////////////////////////////////////
+//// Script
+////
+
+const script = repeat(seq(maybe(comment), declaration)).map(Script.fromSequence);
+
+////////////////////////////////////////////////////////////////
+//// Entry
+////
+
+const entry = alt(
+  expression.map((value) => ({ kind: 'expression' as const, value })),
+  script.map((value) => ({ kind: 'script' as const, value }))
+);
+
+export const parser = new Parser(entry, [ws, ignoredComment]);
