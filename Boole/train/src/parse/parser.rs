5c4310dfb9503debd487c471e9a6eb4cb1ba86a1
jonathan <jonathantbrouwer@gmail.com>
2021-08-21 11:03:49 +0200

    Readable parse errors

--- Boole/train/src/parse/parser.rs
+++ Boole/train/src/parse/parser.rs
@@ -1,3 +1,5 @@
+use std::io::repeat;
+
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct Span {
     pub start: usize,
@@ -16,8 +18,23 @@ pub struct ParserError {
 }
 impl ParserError {
     pub fn print(&self, input: &str) {
-        println!("Parse error!");
-        println!("At [{}-{}]: '{}'", self.span.start, self.span.end, &input[self.span.start..self.span.end]);
+        //Find before
+        let mut line_start = self.span.start;
+        while line_start > 0 && input[(line_start - 1)..].chars().next().unwrap() != '\n' {
+            line_start -= 1;
+        }
+
+        //Find after
+        let mut line_end = self.span.end;
+        while line_end < input.len() && input[line_end..].chars().next().unwrap() != '\n' {
+            line_end += 1;
+        }
+
+        println!("Parse error in line:");
+        println!("{}", &input[line_start..line_end]);
+        print!("{: <1$}", "", self.span.start - line_start);
+        print!("{:^<1$}", "", self.span.end - self.span.start);
+        println!("{: <1$}", "", line_end - self.span.end);
         println!("{}", self.error);
     }
 }
