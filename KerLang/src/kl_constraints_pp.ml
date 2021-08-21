df9e9283cd68b4e3e3790124174aa9d502245abe
anima-libera <anim.libera@gmail.com>
2021-08-22 00:44:10 +0200

    Renamed things in a pp

--- KerLang/src/kl_constraints_pp.ml
+++ KerLang/src/kl_constraints_pp.ml
@@ -1,16 +1,16 @@
 
-open Kl_expr
+open Kl_constraints
 
 let pp_value oc (value: value) = 
   match value with
-  | ARG(id) -> Printf.fprintf oc "x%d" id
-  | CONST(value) -> Printf.fprintf oc "%d" value
-  | VAR(name) -> Printf.fprintf oc "%s" name
-  | SOMETHING -> Printf.fprintf oc "%s" "something"
+  | Arg(id) -> Printf.fprintf oc "x%d" id
+  | Cst(value) -> Printf.fprintf oc "%d" value
+  | Var(name) -> Printf.fprintf oc "%s" name
+  | Hole -> Printf.fprintf oc "%s" "something"
 
-let pp_value_operation oc (vo: value operation) = 
-  match vo with
-  | IF(cond, ifcase, elsecase) -> begin
+let pp_operation oc (operation: operation) = 
+  match operation with
+  | If(cond, ifcase, elsecase) -> begin
       Printf.fprintf oc "%s" "(if ";
       pp_value oc cond;
       Printf.fprintf oc "%s" " then ";
@@ -19,40 +19,40 @@ let pp_value_operation oc (vo: value operation) =
       pp_value oc elsecase;
       Printf.fprintf oc "%s" ")"
     end
-  | SUM(a, b) -> begin
+  | Sum(a, b) -> begin
       Printf.fprintf oc "%s" "(";
       pp_value oc a;
       Printf.fprintf oc "%s" " + ";
       pp_value oc b;
       Printf.fprintf oc "%s" ")"
     end
-  | DIFF(a, b) -> begin
+  | Diff(a, b) -> begin
       Printf.fprintf oc "%s" "(";
       pp_value oc a;
       Printf.fprintf oc "%s" " - ";
       pp_value oc b;
       Printf.fprintf oc "%s" ")"
     end
-  | PROD(a, b) -> begin
+  | Prod(a, b) -> begin
       Printf.fprintf oc "%s" "(";
       pp_value oc a;
       Printf.fprintf oc "%s" " * ";
       pp_value oc b;
       Printf.fprintf oc "%s" ")"
     end
-  | DIV(a, b) -> begin
+  | Div(a, b) -> begin
       Printf.fprintf oc "%s" "(";
       pp_value oc a;
       Printf.fprintf oc "%s" " / ";
       pp_value oc b;
       Printf.fprintf oc "%s" ")"
     end
-  | APPLY(func_name, value_list) -> begin
+  | App(func_name, value_list) -> begin
       Printf.fprintf oc "(%s" func_name;
       List.iter (fun value -> Printf.fprintf oc "%s" " "; pp_value oc value) value_list;
       Printf.fprintf oc "%s" ")"
     end
-  | SELF(value_list) -> begin
+  | Rec(value_list) -> begin
       Printf.fprintf oc "%s" "(recursive_call";
       List.iter (fun value -> Printf.fprintf oc "%s" " "; pp_value oc value) value_list;
       Printf.fprintf oc "%s" ")"
@@ -61,23 +61,23 @@ let pp_value_operation oc (vo: value operation) =
 let pp_expr oc (expr: expr) = 
   match expr with
   | Leaf(value) -> pp_value oc value
-  | Node(value_operation) -> pp_value_operation oc value_operation
+  | Node(operation) -> pp_operation oc operation
 
-let pp_expr_statement oc (es: expr statement) =
-  match es with
-  | TAKES(how_many_args) -> 
+let pp_cconstraint oc (cc: cconstraint) =
+  match cc with
+  | Takes(how_many_args) -> 
     Printf.fprintf oc "- Takes %d arguments\n" how_many_args
-  | LET(var_name, expr) -> begin
+  | Let(var_name, expr) -> begin
       Printf.fprintf oc "- Let %s be " var_name;
       pp_expr oc expr
     end
-  | RETURNS(expr) -> begin
+  | Returns(expr) -> begin
       Printf.fprintf oc "%s" "- Returns ";
       pp_expr oc expr
     end
-  | USES(expr_list) -> begin
+  | Uses(expr_list) -> begin
       Printf.fprintf oc "%s" "- Uses";
       List.iter (fun expr -> Printf.fprintf oc "%s" "\n  * "; pp_expr oc expr) expr_list;
     end
-  | NONE -> 
+  | Nothing -> 
     Printf.fprintf oc "- Has comments for humans\n"
