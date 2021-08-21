8e618c630d3121905f2c63178af1fd39ae96fa4e
grossamos <email@amosgross.com>
2021-08-21 17:48:47 +0200

    refactored parser

--- 〈( ^.^)ノ/parser/parser.go
+++ 〈( ^.^)ノ/parser/parser.go
@@ -4,34 +4,38 @@ import (
 	"github.com/grossamos/jam0001/shared"
 )
 
-func GenerateAst(toks *[]shared.Token) []shared.Node {
+type Parser struct {
+	toks []shared.Token
+}
+
+func (p *Parser) make_ast() []shared.Node {
 	out := make([]shared.Node, 0, 256)
 
-	for len(*toks) != 0 {
-		tok := (*toks)[0]
+	for len(p.toks) != 0 {
+		tok := p.toks[0]
 
 		switch tok.Type {
 		case shared.TTlparen, shared.TTopenBlock:
-			*toks = (*toks)[1:]
+			p.toks = p.toks[1:]
 
 			out = append(out,
 				shared.Node{
 					IsExpression: true,
-					Children:     GenerateAst(toks)})
+					Children:     p.make_ast()})
 
 			continue
 
 		case shared.TTrparen, shared.TTcloseBlock:
-			*toks = (*toks)[1:]
+			p.toks = p.toks[1:]
 			return out
 
 		case shared.TTinstruction: // (instruction (arguments))
-			*toks = (*toks)[1:]
+			p.toks = p.toks[1:]
 
-			if len(*toks) > 0 &&
-				(*toks)[0].Type == shared.TTlparen {
-				
-				*toks = (*toks)[1:]
+			if len(p.toks) > 0 &&
+				p.toks[0].Type == shared.TTlparen {
+
+				p.toks = p.toks[1:]
 			}
 
 			out = append(out,
@@ -39,22 +43,22 @@ func GenerateAst(toks *[]shared.Token) []shared.Node {
 					IsExpression: true,
 					Children: append(
 						[]shared.Node{{Val: tok}},
-						GenerateAst(toks)...)})
+						p.make_ast()...)})
 
 			continue
 
 		case shared.TTstring, shared.TTref:
-			if len(*toks) > 1 &&
-				(*toks)[1].Type == shared.TTlparen {
+			if len(p.toks) > 1 &&
+				(p.toks)[1].Type == shared.TTlparen {
 
-				*toks = (*toks)[1:]
+				p.toks = (p.toks)[1:]
 
 				out = append(out,
 					shared.Node{
 						IsExpression: true,
 						Children: append(
 							[]shared.Node{{Val: tok}},
-							GenerateAst(toks)...)})
+							p.make_ast()...)})
 
 				continue
 			} else {
@@ -75,30 +79,35 @@ func GenerateAst(toks *[]shared.Token) []shared.Node {
 					out[index]}}
 
 		case shared.TTwhile:
-			if len(*toks) <= 1 {
+			if len(p.toks) <= 1 {
 				break
 			}
 
-			*toks = (*toks)[1:]
+			p.toks = p.toks[1:]
 			out = append(out,
 				shared.Node{
 					IsExpression: true,
 					Children: append(
 						append(
 							[]shared.Node{{Val: tok}},
-							GenerateAst(toks)...),
-						GenerateAst(toks)...)})
+							p.make_ast()...),
+						p.make_ast()...)})
 
 		default:
 			out = append(out, shared.Node{Val: tok})
 		}
 
-		if len(*toks) < 1 {
+		if len(p.toks) < 1 {
 			break
 		}
 
-		*toks = (*toks)[1:]
+		p.toks = p.toks[1:]
 	}
 
 	return out
+}
+
+func GenerateAst(toks []shared.Token) []shared.Node {
+	parser := Parser{toks}
+	return parser.make_ast()
 }
