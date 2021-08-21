cf7a85b05c1bce16a1b5a6cbe55652cb748fa6ae
Lorne Hyde <lorne.ab.hyde@gmail.com>
2021-08-21 16:14:33 +0100

    Update tokenise.py

--- The-Literals/tokenise.py
+++ The-Literals/tokenise.py
@@ -5,29 +5,33 @@ from enum import Enum, auto
 
 
 class Token(Enum):
-    and_call_it = auto()
-    binop = auto()
-    bof = auto()
-    code = auto()
-    comparison = auto()
-    done = auto()
-    ditto = auto()
-    dot = auto()
-    eof = auto()
-    eol = auto()
-    function = auto()
-    function_name = auto()
-    header_end = auto()
-    hfill = auto()
-    identifier = auto()
-    if_keyword = auto()
-    negative = auto()
-    number = auto()
-    param = auto()
-    returnvar = auto()
-    setvar = auto()
-    then = auto()
-    to = auto()
+    AND = auto()
+    AND_CALL_IT = auto()
+    AS = auto()
+    BINOP = auto()
+    BOF = auto()
+    CODE = auto()
+    COMPARISON = auto()
+    DITTO = auto()
+    DOT = auto()
+    END_DEF = auto()
+    EOF = auto()
+    EOL = auto()
+    FUNCTION = auto()
+    FUNCTION_NAME = auto()
+    HEADER_END = auto()
+    HFILL = auto()
+    IDENTIFIER = auto()
+    IF_KEYWORD = auto()
+    LEAVE_FUNC = auto()
+    NUMBER = auto()
+    NEGATIVE = auto()
+    PARAM = auto()
+    RETURNVAR = auto()
+    SETVAR = auto()
+    THEN = auto()
+    TO = auto()
+    WITH = auto()
 
 
 class Tokeniser:
@@ -52,12 +56,15 @@ class Tokeniser:
         peeked = self.text[self.current : self.current + n]
         return peeked
 
-    def advance_if(self, *args):
+    def advance_if(self, *args, case_sensitive=False):
         # Sort args into reversed order of length for greedy matching.
         sorted_args = sorted(args, key=len, reverse=True)
         for s in sorted_args:
             peeked = self.peek(len(s))
-            if peeked.lower() == s.lower():
+            if not case_sensitive:
+                peeked = peeked.lower()
+                s = s.lower()
+            if peeked == s:
                 self.current += len(s)
                 return True
         return False
@@ -65,14 +72,14 @@ class Tokeniser:
     def function(self):
         while self.peek() == "*":
             self.advance()
-        return Token.function
+        return Token.FUNCTION
 
     def function_name(self):
         # A function name is terminated by a full stop and cannot go onto the next line.
         while self.peek() not in (".", "\n", ""):
             self.advance()
         if not self.done() and self.text[self.current] == ".":
-            return Token.function_name
+            return Token.FUNCTION_NAME
 
     def ignore(self):
         while self.peek() != "\n":
@@ -83,76 +90,88 @@ class Tokeniser:
         while p.isalnum() or p == "'":
             self.advance()
             p = self.peek()
-        return Token.identifier
+        return Token.IDENTIFIER
 
     def number(self):
         while self.peek().isdigit():
             self.advance()
-        return Token.number
+        return Token.NUMBER
 
     def scan_token(self):
         ch = self.advance()
         if ch in "\n":
             self.line += 1
-            return Token.eol
+            return Token.EOL
 
         # Contextual scanning for tokens that can appear at the start of a line.
-        if self.last in (Token.bof, Token.eol):
+        if self.last in (Token.BOF, Token.EOL):
             # Is it a function?
             if ch == "/" and self.peek(2) == "**":
                 return self.function()
 
             # Is it the end of a function signature?
             if self.advance_if(" */", "*/"):
-                return Token.header_end
+                return Token.HEADER_END
 
             # Is it a header filler?
             if ch == " " and self.peek() == "*":
                 self.advance()
-                return Token.hfill
+                return Token.HFILL
 
             # Is it the start of a code line?
             if self.advance_if("//", " //"):
-                return Token.code
+                return Token.CODE
 
             # Is it an ignored line?
             if ch.isalpha():
                 self.ignore()
 
-        elif self.last == Token.hfill:
+        elif self.last == Token.HFILL:
             if self.advance_if("@param"):
-                return Token.param
+                return Token.PARAM
 
             if self.advance_if("@return"):
-                return Token.returnvar
+                return Token.RETURNVAR
 
             # Is it a function name?
             if ch.isalpha():
                 return self.function_name()
         else:
-            if self.advance_if("and we're done"):
-                return Token.done
+            if self.advance_if("and we're done", case_sensitive=True):
+                return Token.LEAVE_FUNC
+
+            if self.advance_if("And we're done", case_sensitive=True):
+                return Token.END_DEF
 
             if self.advance_if("If"):
-                return Token.if_keyword
+                return Token.IF_KEYWORD
 
             if self.advance_if("then"):
-                return Token.then
+                return Token.THEN
 
             if self.advance_if("set"):
-                return Token.setvar
+                return Token.SETVAR
 
             if self.advance_if("to"):
-                return Token.to
+                return Token.TO
 
             if self.advance_if("ditto"):
-                return Token.ditto
+                return Token.DITTO
 
             if self.advance_if("and call it"):
-                return Token.and_call_it
+                return Token.AND_CALL_IT
 
+            if self.advance_if("and"):
+                return Token.AND
+
+            if self.advance_if("with"):
+                return Token.WITH
+
+            if self.advance_if("as"):
+                return Token.AS
+                
             if ch == ".":
-                return Token.dot
+                return Token.DOT
 
             # Is it a positive number?
             if ch.isdigit():
@@ -160,13 +179,13 @@ class Tokeniser:
 
             # Is it a negative number?
             if ch == "-" and self.peek().isdigit():
-                return Token.negative
+                return Token.NEGATIVE
 
             # Is it a binary operator on numbers?
             is_operator = ch in "+-/*%"
             is_spaced = self.peek().isspace()
             if is_operator and is_spaced:
-                return Token.binop
+                return Token.BINOP
 
             # Is it a boolean comparison?
             if self.advance_if(
@@ -176,7 +195,7 @@ class Tokeniser:
                 "is less than or equal to",
                 "is greater than or equal to",
             ):
-                return Token.comparison
+                return Token.COMPARISON
 
             # Is it an identifier? This MUST go at the end because of how vague it is.
             if ch.isalpha():
@@ -188,7 +207,7 @@ class Tokeniser:
         self.start = 0
         self.current = 0
 
-        token = Token.bof
+        token = Token.BOF
         yield (token, "")
         self.last = token
 
@@ -199,7 +218,7 @@ class Tokeniser:
                 yield (token, self.text[self.start : self.current])
                 self.last = token
 
-        yield (Token.eof, "")
+        yield (Token.EOF, "")
         self.last = token
 
     def __iter__(self):
