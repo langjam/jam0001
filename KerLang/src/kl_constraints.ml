29a3ed3e82040900399830c240f062ab5c5d510c
anima-libera <anim.libera@gmail.com>
2021-08-22 17:35:54 +0200

    j

--- KerLang/src/kl_constraints.ml
+++ KerLang/src/kl_constraints.ml
@@ -31,55 +31,55 @@ type function_result =
   | Function of expr
   | Yolo of expr list
 
-  let rec pp_expr oc (expr : expr) =
-    match expr with
-    | Leaf value -> pp_value oc value
-    | Node operation -> pp_operation oc operation
-  
-  and pp_value oc (value: value) =
-    match value with
-    | Arg id -> Printf.fprintf oc "x%d" id
-    | Cst value -> Printf.fprintf oc "%d" value
-    | Var name -> Printf.fprintf oc "%s" name
-    | Hole -> Printf.fprintf oc "{??}"
-  
-  and pp_operation oc (operation : operation) =
-    match operation with
-    | If (cond, ifcase, elsecase) ->
-      Printf.fprintf oc "(if %a then %a else %a)"
-        pp_expr cond
-        pp_expr ifcase
-        pp_expr elsecase
-    | Sum (a, b) ->
-      Printf.fprintf oc "(%a + %a)" pp_expr a pp_expr b
-    | Diff (a, b) ->
-      Printf.fprintf oc "(%a - %a)" pp_expr a pp_expr b
-    | Prod (a, b) ->
-      Printf.fprintf oc "(%a * %a)" pp_expr a pp_expr b
-    | Div (a, b) ->
-      Printf.fprintf oc "(%a / %a)" pp_expr a pp_expr b
-    | App (func_name, value_list) ->
-      Printf.fprintf oc "(%s " func_name;
-      List.iter (fun value -> Printf.fprintf oc "%a " pp_expr value) value_list;
-      Printf.fprintf oc ")"
-    | Rec value_list ->
-      Printf.fprintf oc "(rec ";
-      List.iter (fun value -> Printf.fprintf oc "%a " pp_expr value) value_list;
-      Printf.fprintf oc ")"
-  
-  let pp_cconstraint oc (cc: cconstraint) =
-    match cc with
-    | Takes how_many_args ->
-      Printf.fprintf oc "- Takes %d arguments\n" how_many_args
-    | Let (var_name, expr) ->
-      Printf.fprintf oc "- Let %s be %a" var_name pp_expr expr
-    | Returns expr ->
-      Printf.fprintf oc "- Returns %a" pp_expr expr
-    | Uses expr_list ->
-      Printf.fprintf oc "- Uses";
-      List.iter (fun expr -> Printf.fprintf oc "%s" "\n  * "; pp_expr oc expr) expr_list
-    | Nothing ->
-      Printf.fprintf oc "- Has comments for humans\n"  
+let rec pp_expr oc (expr : expr) =
+  match expr with
+  | Leaf value -> pp_value oc value
+  | Node operation -> pp_operation oc operation
+
+and pp_value oc (value: value) =
+  match value with
+  | Arg id -> Printf.fprintf oc "x%d" id
+  | Cst value -> Printf.fprintf oc "%d" value
+  | Var name -> Printf.fprintf oc "%s" name
+  | Hole -> Printf.fprintf oc "{??}"
+
+and pp_operation oc (operation : operation) =
+  match operation with
+  | If (cond, ifcase, elsecase) ->
+    Printf.fprintf oc "(if %a then %a else %a)"
+      pp_expr cond
+      pp_expr ifcase
+      pp_expr elsecase
+  | Sum (a, b) ->
+    Printf.fprintf oc "(%a + %a)" pp_expr a pp_expr b
+  | Diff (a, b) ->
+    Printf.fprintf oc "(%a - %a)" pp_expr a pp_expr b
+  | Prod (a, b) ->
+    Printf.fprintf oc "(%a * %a)" pp_expr a pp_expr b
+  | Div (a, b) ->
+    Printf.fprintf oc "(%a / %a)" pp_expr a pp_expr b
+  | App (func_name, value_list) ->
+    Printf.fprintf oc "(%s " func_name;
+    List.iter (fun value -> Printf.fprintf oc "%a " pp_expr value) value_list;
+    Printf.fprintf oc ")"
+  | Rec value_list ->
+    Printf.fprintf oc "(rec ";
+    List.iter (fun value -> Printf.fprintf oc "%a " pp_expr value) value_list;
+    Printf.fprintf oc ")"
+
+let pp_cconstraint oc (cc: cconstraint) =
+  match cc with
+  | Takes how_many_args ->
+    Printf.fprintf oc "- Takes %d arguments\n" how_many_args
+  | Let (var_name, expr) ->
+    Printf.fprintf oc "- Let %s be %a" var_name pp_expr expr
+  | Returns expr ->
+    Printf.fprintf oc "- Returns %a" pp_expr expr
+  | Uses expr_list ->
+    Printf.fprintf oc "- Uses";
+    List.iter (fun expr -> Printf.fprintf oc "%s" "\n  * "; pp_expr oc expr) expr_list
+  | Nothing ->
+    Printf.fprintf oc "- Has comments for humans\n"  
 
 type comment_function = {
   name : string;
@@ -111,11 +111,11 @@ let print_comment_function {name; n_args; declarations; result; _} =
     Printf.printf "- constraints for %s are'nt sufficient to build a function\n" name;
     Printf.printf "- known context to synthesize a function:\n";
     List.iteri (fun i expr ->
-      Printf.printf "  %d : %a\n" i pp_expr expr) ctx
+        Printf.printf "  %d : %a\n" i pp_expr expr) ctx
   | Function e ->
     Printf.printf "- known context:\n";
     List.iter (fun (name, expr) ->
-      Printf.printf "  \"%s\" : %a\n" name pp_expr expr) declarations;
+        Printf.printf "  \"%s\" : %a\n" name pp_expr expr) declarations;
     Printf.printf "- generated function is:\n";
     Printf.printf "  %a\n" pp_expr e;
     if has_holes e then begin
@@ -282,11 +282,11 @@ let generate_function (Spec (_, name, comment)) =
         in build_function {f with result = r} q
   in
   let res = build_function {
-    name; n_args = 0; declarations = []; result = Yolo []
-  } comments
+      name; n_args = 0; declarations = []; result = Yolo []
+    } comments
   in
   print_comment_function res; res
-  
+
 
 let rec complete_holes decl (remain : expr list) = function
   | Leaf Hole ->
