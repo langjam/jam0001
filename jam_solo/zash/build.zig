11453c8c68df1f4287eac758ea66728945fd819a
Jose Colon Rodriguez <jec.rod@gmail.com>
2021-08-21 20:12:50 -0400

    Rename to zash

--- jam_solo/zash/build.zig
+++ jam_solo/zash/build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("slash", "src/main.zig");
+    const exe = b.addExecutable("zash", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
