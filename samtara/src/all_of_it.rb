8276bdfbab96da305ba9fa6c2cedbcd320e9be35
Samuel Doiron <sam.doiron96@gmail.com>
2021-08-22 11:22:33 -0400

    Rewrite to always use shortest

--- samtara/src/all_of_it.rb
+++ samtara/src/all_of_it.rb
@@ -32,29 +32,48 @@ def join_pattern(pattern)
   end.join
 end
 
+class MatchData
+  def match_length
+    self.end(0) - self.begin(0)
+  end
+end
+
 def run(code)
   input, rules = code.split(/\n-{3,}/m)
   rules = parse(rules || '')
   last_input = nil
-  debug "-> #{input}"
+  debug input
+  debug
   while input != last_input do
     last_input = input.clone
-    rules.find do |rule|
-      rule_regex = to_regex(rule[:header])
-      shortest_match = input.matches(rule_regex).min do |a, b|
-        a.length <=> b.length
+    found = rules.map do |rule|
+      shortest_match = input.matches(rule[:header_regex]).min do |a, b|
+        a.match_length <=> b.match_length
       end
-      if shortest_match
-        debug "match: #{join_pattern(rule[:header])}\t#{shortest_match.inspect}"
-        rewrite = run_rule(shortest_match, rule[:rewrite])
-        input[shortest_match.begin(0)...shortest_match.end(0)] = rewrite
-        debug "===> #{input}"
-        debug
-        true
-      else
-        false
+      [rule, shortest_match]
+    end.reject { |(_, match)| match.nil? }
+      .min_by do |(rule, match)|
+        [match.match_length, match.named_captures.keys.length]
       end
+
+    if found
+      found_rule, found_match = found
+      rewrite = run_rule(found_match, found_rule[:rewrite])
+      if DEBUG
+        debug "via #{join_pattern(found_rule[:header])} #{found_match.named_captures.inspect} (length #{found_match.match_length})"
+        before_input = input.clone
+        after_input = input.clone
+        current = before_input[found_match.begin(0)...found_match.end(0)]
+        before_input[found_match.begin(0)...found_match.end(0)] = "\e[31m#{current}\e[0m"
+        puts before_input
+        puts
+        after_input[found_match.begin(0)...found_match.end(0)] = "\e[32m#{rewrite}\e[0m"
+        puts "#{after_input}"
+        puts
+      end
+      input[found_match.begin(0)...found_match.end(0)] = rewrite
     end
+    gets
   end
   input
 end
@@ -119,8 +138,10 @@ def parse(rules)
   end.compact.reduce([]) do |rules, token|
     case token.first
     when :header
+      pattern = parse_pattern(token.last)
       rules << {
-        header: parse_pattern(token.last),
+        header: pattern,
+        header_regex: to_regex(pattern),
         rewrite: nil
       }
     when :rewrite
@@ -330,17 +351,8 @@ TESTS = [
     code: <<~BO,
     [||||] - [||] - [|]
     ----------------------
-    [<X>|] - [<Y>|]
-      [<X>] - [<Y>]
-
-    [<X>|] - [|]
-      [<X>] - []
-
-    [<X>] - []
+    [<X><Y>] - [<Y>]
       [<X>]
-
-    [] - [<Y>]
-      []
     BO
     result: '[|]'
   },
@@ -371,13 +383,13 @@ TESTS = [
   {
     name: 'All in a row',
     code: <<~BO,
-    print each letter of hello world
+    print each letter of hello world.
     -----------
-    print each letter of <x><X>
-      <x> print each letter of <X>
+    print each letter of <x><X>.
+      <x> print each letter of <X>.
 
-    print each letter of 
-      
+    print each letter of <x>.
+      <x>
     BO
     result: "h e l l o   w o r l d"
   },
@@ -395,29 +407,146 @@ TESTS = [
     result: "bar"
   },
   {
-    name: 'Fizzbuzz',
-    skip: true,
+    name: 'Fizzbuzz, Part one: fizz',
     code: <<~BO,
-    fizz-num [||].
+    fizz-check fizz [||||||] [|||].
     ----
-    fizz-num [<N>].
-      ?fizz-check fizz [<N>] [|||]:[<N>]
+    fizz-check <M> [<R><D>] [<D>].
+      fizz-check <M> [<R>] [<D>].
 
-    fizz-check <M> [<N><D>] [<D>]
-      fizz-check <M> [<N>] [<D>]
-
-    fizz-check <M> [] [<D>]
+    fizz-check <M> [<D>] [<D>].
       <M>
 
-    fizz-check <M> [<N>] [<D>]
-      
-    ?:<Y>
-      <Y>
-
-    ?<X>:<Y>
-      <X>
+    fizz-check <M> [<R>] [<D>].
+      fail
     BO
-    result: "[|]\n[||]\nFizz\n[||||]\nBuzz"
+    result: "fizz"
+  },
+  {
+    name: 'Fizzbuzz, Part two: also buzz',
+    code: <<~BO,
+    fizz-num [|];
+    fizz-num [||];
+    fizz-num [|||];
+    fizz-num [||||];
+    fizz-num [|||||];
+    fizz-num [||||||];
+    fizz-num [|||||||];
+    fizz-num [||||||||];
+    fizz-num [|||||||||];
+    fizz-num [||||||||||];
+    fizz-num [|||||||||||];
+    fizz-num [||||||||||||];
+    fizz-num [|||||||||||||];
+    fizz-num [||||||||||||||];
+    fizz-num [|||||||||||||||];
+
+    ----
+
+    fizz-num [<N>];
+      ?fizz-check "fizz" [<N>] [|||].fizz-check "buzz" [<N>] [|||||].:[<N>]!
+
+    fizz-check "<M>" [<R><D>] [<D>].
+      fizz-check "<M>" [<R>] [<D>].
+
+
+    fizz-check "<M>" [<D>] [<D>].
+      <M>
+
+    fizz-check "<M>" [<R>] [<R><D>].
+      
+
+    ?<A>:<B>!
+      <A>
+
+    ?:<B>!
+      <B>
+    BO
+    result: <<~RES
+    [|]
+    [||]
+    fizz
+    [||||]
+    buzz
+    fizz
+    [|||||||]
+    [||||||||]
+    fizz
+    buzz
+    [|||||||||||]
+    fizz
+    [|||||||||||||]
+    [||||||||||||||]
+    fizzbuzz
+    RES
+  },
+  {
+    name: 'Decimal to unary',
+    code: <<~BO,
+    dec(3) dec(21)
+    --------------
+
+    dec(<N>)
+      [decb(<N>, |)]
+
+    decb(<n>, <M>)
+      rep(<n>, <M>)
+
+    decb(<R><n>, <M>)
+      decb(<R>, <M><M><M><M><M><M><M><M><M><M>)rep(<n>, <M>)
+
+    rep(0, <B>)
+      
+    rep(1, <B>)
+      <B>
+    rep(2, <B>)
+      <B><B>
+    rep(3, <B>)
+      <B><B><B>
+    rep(4, <B>)
+      <B><B><B><B>
+    rep(5, <B>)
+      <B><B><B><B><B>
+    rep(6, <B>)
+      <B><B><B><B><B><B>
+    rep(7, <B>)
+      <B><B><B><B><B><B><B>
+    rep(8, <B>)
+      <B><B><B><B><B><B><B><B>
+    rep(9, <B>)
+      <B><B><B><B><B><B><B><B><B>
+
+    BO
+    result: '[|||] [|||||||||||||||||||||]'
+  },
+
+  #Current pattern	      111	110	101	100	011	010	001	000
+  #New state for center cell	0	1	1	0	1	1	1	0
+  {
+    name: 'Rule 110',
+    focus: true,
+    code: <<~BO,
+    ^|0000000000000010000000000000000000$
+    ---
+
+    0|00
+      00|0
+    0|01
+      01|1
+    0|10
+      01|0
+    0|11
+      01|1
+    1|00
+      10|0
+    1|01
+      11|1
+    1|10
+      11|0
+    1|11
+      10|1
+    BO
+    result: '?'
   }
 ]
 def test
@@ -451,14 +580,17 @@ def run_test(test)
       puts
       puts "Expected:"
       puts '```'
-      puts test[:result].inspect
+      puts test[:result]
       puts '```'
       puts
       puts "Actual:"
       puts '```'
-      puts actual.inspect
+      puts actual
       puts '```'
       puts '=========================='
+      puts test[:result].inspect
+      puts "but got"
+      puts actual.inspect
       false
     else
       true
