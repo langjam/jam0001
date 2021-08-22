43477f08585e03463de9b60b46422dbce55db59a
grossamos <email@amosgross.com>
2021-08-22 17:42:45 +0200

    created usable main.go

--- 〈( ^.^)ノ/main.go
+++ 〈( ^.^)ノ/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"io/ioutil"
@@ -16,22 +17,37 @@ import (
 
 func main() {
 	rand.Seed(time.Now().Unix())
+	debugMode := false
+
+	// do initial checks
+	if len(os.Args) == 1 {
+		fmt.Println("Please provide a file to run")
+	}
+	if len(os.Args) > 2 && contains(os.Args, "--debug") {
+		debugMode = true
+	}
 
 	// load test code from disk
-	dat, err := ioutil.ReadFile("examples/100doors.sml")
+	dat, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 	code := string(dat)
-	fmt.Println(code)
+	if debugMode {
+		fmt.Println(code)
+	}
 
 	// run lexer
 	toks := lexer.RunLexer(code)
-	fmt.Println(toks)
+	if debugMode {
+		fmt.Println(toks)
+	}
 
 	// run parser
 	nodes, comments, err := parser.GenerateAst(toks)
-	shared.Node{IsExpression: true, Children: nodes}.Print("")
+	if debugMode {
+		shared.Node{IsExpression: true, Children: nodes}.Print("")
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -41,4 +57,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
