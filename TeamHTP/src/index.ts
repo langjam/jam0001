51baef635fe6a45a0479319d6e8d6832477075ec
Dominic Tassio <dominic.tassio@gmail.com>
2021-08-22 11:03:52 -0500

    Handler repl parse error

--- TeamHTP/src/index.ts
+++ TeamHTP/src/index.ts
@@ -4,7 +4,7 @@ import {startRepl} from './Repl'
 import Runtime from './Runtime'
 import {saveToDisk} from './Runtime/File'
 import {makeTransformer} from "./transformer";
-import {get_parser} from "./mouthful_repl";
+import {get_parser, UnexpectedToken} from "./mouthful_repl";
 
 const srcPath = process.argv[2];
 
@@ -13,14 +13,22 @@ const src = fs.readFileSync(srcPath).toString()
 const runtime = new Runtime(src)
 
 startRepl(async (input) => {
-    const evalScope = runtime.generateEvalScope()
-    const transformer = makeTransformer(evalScope)
-    const parser = get_parser({transformer})
-    const js = parser.parse(input)
-    //console.log(js)
-    const out = function (js: string) {
-        return eval(js)
-    }.call(evalScope, js)
-    console.log(out)
-    await saveToDisk(srcPath, runtime)
+    try {
+        const evalScope = runtime.generateEvalScope()
+        const transformer = makeTransformer(evalScope)
+        const parser = get_parser({transformer})
+        const js = parser.parse(input)
+        //console.log(js)
+        const out = function (js: string) {
+            return eval(js)
+        }.call(evalScope, js)
+        console.log(out)
+        await saveToDisk(srcPath, runtime)
+    } catch (e) {
+        if (e instanceof UnexpectedToken) {
+            console.log(`Syntax error! :(`)
+        } else {
+            throw e;
+        }
+    }
 }).catch(console.log)
