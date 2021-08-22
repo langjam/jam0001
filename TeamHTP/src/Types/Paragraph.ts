3fae8f3abcb8db93a134408cfe77ff3f7d5abdde
Tianyi Ma <tn.ma@outlook.com>
2021-08-21 21:00:58 -0500

    Appending to paragraphs

--- TeamHTP/src/Types/Paragraph.ts
+++ TeamHTP/src/Types/Paragraph.ts
@@ -1,9 +1,17 @@
 import Base from './Base'
 import {Paragraph as MdastParagraph} from 'mdast'
+import {mdastToMd, mdToMdast} from "../Markdown";
 
 class Paragraph extends Base {
     getMdastContent(): MdastParagraph {
         return <MdastParagraph>this.mdastContent
+    }
+
+    append(item: string): void {
+        let md = mdastToMd(this.getMdastContent(), true)
+        md += item
+        const serializedChildren = (<MdastParagraph>mdToMdast(md).children[0]).children
+        this.getMdastContent().children = serializedChildren
     }
 }
 
